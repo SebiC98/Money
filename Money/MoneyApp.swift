3982955d055a9aa98cd792b2c3f0a45cd281ce17
SebiC98 <sebastiancioata98@gmail.com>
2023-03-03 17:17:55 +0200

    First commit

---
+++ Money/MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyApp.swift
+//  Money
+//
+//  Created by Sebastian Cioată on 03.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
